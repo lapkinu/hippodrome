0ab1f4877ffaac6cc5b7bb7d2ea76707da215ff3
lapkin <lapkinu@gmail.com>
2024-08-22 01:07:48 +0300

    "added constructor test"

---
+++ src/test/java/HorseTest.java
@@ -0,0 +1,62 @@
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+class HorseTest {
+
+    // Test the constructor name parameter null value exception case
+    @Test
+    void testConstructorNameNull() {
+        assertThrows(IllegalArgumentException.class, () -> new Horse(null, 10));
+    }
+
+    // "Name cannot be null." exception message test
+    @Test
+    void testConstructorNameNullMessage() {
+        Throwable exception = assertThrows(IllegalArgumentException.class, () -> new Horse(null, 10));
+        assertEquals("Name cannot be null.", exception.getMessage());
+    }
+
+    //IllegalArgumentException("Name cannot be blank.") exception case test parameterized test
+    @ParameterizedTest
+    @ValueSource(strings = {"", " ", "  ", "   "})
+    void testConstructorNameBlank(String name) {
+        assertThrows(IllegalArgumentException.class, () -> new Horse(name, 10));
+    }
+
+    // "Name cannot be blank." exception message parameterized test
+    @ParameterizedTest
+    @ValueSource(strings = {"", " ", "  ", "   "})
+    void testConstructorNameBlankMessage(String name) {
+        Throwable exception = assertThrows(IllegalArgumentException.class, () -> new Horse(name, 10));
+        assertEquals("Name cannot be blank.", exception.getMessage());
+    }
+
+    // Test the constructor speed parameter negative value exception case
+    @Test
+    void testConstructorSpeedNegative() {
+        assertThrows(IllegalArgumentException.class, () -> new Horse("Bucephalus", -10));
+    }
+
+    // "Speed cannot be negative." exception message test
+    @Test
+    void testConstructorSpeedNegativeMessage() {
+        Throwable exception = assertThrows(IllegalArgumentException.class, () -> new Horse("Bucephalus", -10));
+        assertEquals("Speed cannot be negative.", exception.getMessage());
+    }
+
+    // Test the constructor distance parameter negative value exception case
+    @Test
+    void testConstructorDistanceNegative() {
+        assertThrows(IllegalArgumentException.class, () -> new Horse("Bucephalus", 10, -10));
+    }
+
+    // "Distance cannot be negative." exception message test
+    @Test
+    void testConstructorDistanceNegativeMessage() {
+        Throwable exception = assertThrows(IllegalArgumentException.class, () -> new Horse("Bucephalus", 10, -10));
+        assertEquals("Distance cannot be negative.", exception.getMessage());
+    }
+
+}
